ba7f79a9a9911b5c4af061cf01093acb22df63ba
Nevalainen Susanna <susanna.nevalainen@aalto.fi>
2018-03-24 13:54:36 +0200

    initial commit

---
+++ src/containers/about/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+    margin: 10
+  },
+});
+
+let id = 0;
+function createData(vessel, containers) {
+  id += 1;
+  return { id, vessel, containers,};
+}
+
+const data = [
+  createData('vessel 1', [1, 2]),
+  createData('vessel 2', [3]),
+  createData('vessel 3', [5]),
+];
+
+function SimpleTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Vessels</TableCell>
+            <TableCell>Containers</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(n => {
+            return (
+              <TableRow key={n.id}>
+                <TableCell>{n.vessel}</TableCell>
+                <TableCell>{n.containers}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+SimpleTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTable);
